ea3d2547809b0c4feb1b8448335d934ad2b9305d
asselism <assel.ismoldayeva@xaira.com>
2026-03-25 23:01:11 -0700

    fix name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/habit-tracker/',
+  base: '/another-habit-tracker/',
   plugins: [react()],
   server: {
     watch: {
